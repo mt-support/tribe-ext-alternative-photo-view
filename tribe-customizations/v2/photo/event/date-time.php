cbcce092a6a3228476e266a2db35a4c6ac8c7ea4
András Guseo <andras@divi-magazine.com>
2020-10-28 11:53:57 +0100

    date-time.php template cleanup

--- tribe-customizations/v2/photo/event/date-time.php
+++ tribe-customizations/v2/photo/event/date-time.php
@@ -2,41 +2,43 @@
 /**
  * View: Photo View - Single Event Date Time
  *
- * Override this template in your own theme by creating a file at:
- * [your-theme]/tribe/events-pro/v2/photo/event/date-time.php
+ * This is a template override of the file at:
+ * events-calendar-pro/src/views/v2/photo/event/date-time.php
  *
  * See more documentation about our views templating system.
  *
- * @link http://m.tri.be/1aiy
+ * @link    http://m.tri.be/1aiy
  *
- * @since 5.0.0
- * @since 5.1.1 Moved icons out to separate templates.
+ * @since   5.0.0
+ * @since   5.1.1 Moved icons out to separate templates.
  *
- * @var WP_Post $event The event post object with properties added by the `tribe_get_event` function.
- * @var obj     $date_formats Object containing the date formats.
+ * @var WP_Post $event            The event post object with properties added by the `tribe_get_event` function.
+ * @var obj     $date_formats     Object containing the date formats.
+ * @var string  $time_format      The time format settings of The Events Calendar
+ * @var string  $display_end_date bool whether the end date of an event should be shown
  *
- * @see tribe_get_event() For the format of the event object.
+ * @see     tribe_get_event() For the format of the event object.
  *
  * @version 5.1.1
  */
 
 $time_format = tribe_get_time_format();
-//$time_format = tribe_get_datetime_format();
-$display_end_date = $event->dates->start_display->format( 'H:i' ) !== $event->dates->end_display->format( 'H:i' );
+//$display_end_date = $event->dates->start_display->format( 'H:i' ) !== $event->dates->end_display->format( 'H:i' );
 $display_end_date = false;
 ?>
 <div class="tribe-events-pro-photo__event-datetime tribe-common-b2">
-	<?php //$this->template( 'photo/event/date-time/featured' ); ?>
-	<?php if ( $event->multiday ) : ?>
-	<?php //echo $event->schedule_details->value();
-	$one = date_create( $event->dates->start_display->format('Y-m-d') );
-	$two =  date_create( $event->dates->end_display->format('Y-m-d') )->modify('+1 day');
-	$nt = date_diff( $one, $two, true );
 
-	if ( ! $event->all_day ) {
-		echo esc_html( $event->dates->start_display->format( $time_format ) ) . ' | ';
-	}
-	echo $nt->format("%a" ) . '-day event';?>
+	<?php
+	if ( $event->multiday ) :
+		$date_one = date_create( $event->dates->start_display->format( 'Y-m-d' ) );
+		$date_two = date_create( $event->dates->end_display->format( 'Y-m-d' ) )->modify( '+1 day' );
+		$nt       = date_diff( $date_one, $date_two, true );
+
+		if ( ! $event->all_day ) {
+			echo esc_html( $event->dates->start_display->format( $time_format ) ) . ' | ';
+		}
+		echo $nt->format( "%a" ) . '-day event';
+		?>
 	<?php elseif ( $event->all_day ) : ?>
 		<time datetime="<?php echo esc_attr( $event->dates->start_display->format( 'Y-m-d' ) ) ?>">
 			<?php esc_attr_e( 'All day', 'tribe-events-calendar-pro' ); ?>
